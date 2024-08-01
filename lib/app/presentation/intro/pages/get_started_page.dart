60f1ac9612e6d13a4ff0d00ad6c06d54b03799fb
Cholponai Amangeldi <cholponaiamangeldi@MacBook-Pro-Cholponai.local>
2024-08-01 23:59:19 +0600

    second page done

--- lib/app/presentation/intro/pages/get_started_page.dart
+++ lib/app/presentation/intro/pages/get_started_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:spotify/core/config/assets/app_images.dart';
+import 'package:spotify/core/config/assets/app_vectors.dart';
+import 'package:svg_flutter/svg.dart';
 
 class GetStartedPage extends StatelessWidget {
   const GetStartedPage({super.key});
@@ -10,9 +12,27 @@ class GetStartedPage extends StatelessWidget {
       body: Stack(
         children: [
           Container(
-            decoration: BoxDecoration(
+            decoration: const BoxDecoration(
                 image: DecorationImage(
                     fit: BoxFit.fill, image: AssetImage(AppImages.introBg))),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 40),
+              child: Column(
+                children: [
+                  Align(
+                      alignment: Alignment.topCenter,
+                      child: SvgPicture.asset(AppVectors.logo)),
+                  const Spacer(),
+                  const Text(
+                    'Enjoy Listening To Music',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: 18),
+                  )
+                ],
+              ),
+            ),
           )
         ],
       ),
